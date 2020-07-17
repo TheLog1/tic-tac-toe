513d7b3e56c8d681df5afe7e394e7354f6a8cd38
TheLog <lgscustomww@gmail.com>
2020-07-17 11:03:33 -0500

    added some css/html for layout of site.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,10 +6,12 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events')
-
+const gameEvents = require('./game/events')
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('click', authEvents.onSignOut)
+  // game events
+  $('#start-game').on('click', gameEvents.onStartGame)
 })
