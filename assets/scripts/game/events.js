ee6cb7361261f6c94dcf5ef6f8888d32ccc09b1e
TheLog <lgscustomww@gmail.com>
2020-07-22 07:38:40 -0500

    trying to fix my picture problem

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -32,13 +32,13 @@ const onCellClick = (e) => {
     api.userChoice(e.target.dataset.cellIndex, currentMove)
       .then(ui.userChoiceSuccess)
       .catch(ui.userChoiceFailure)
-    $('#' + e.target.id).append('<img class="spot-image" src="../assets/images/xGivingit.png" />')
+    $('#' + e.target.id).append('<img class="spot-image" src="../assets/images/XGivingIt.PNG"/>')
     currentMove = '0'
   } else {
     api.userChoice(e.target.dataset.cellIndex, currentMove)
       .then(ui.userChoiceSuccess)
       .catch(ui.userChoiceFailure)
-    $('#' + e.target.id).append('<img class="spot-image" src="../assets/images/oGivingIt.png" />')
+    $('#' + e.target.id).append('<img class="spot-image" src="../assets/images/OGivingIt.PNG"/>')
     currentMove = 'X'
   }
   e.target.style.pointerEvents = 'none'
